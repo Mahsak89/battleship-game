b1b5112843cb8417e41e8c1a8090b8798a1cc7db
Mahsak89 <khoshnoudmahsa@gmail.com>
2023-02-08 13:00:55 +0000

    make  creat_ships function and import from random

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
 
 class GameBoard:
     def __init__(self, board):
@@ -15,9 +17,21 @@ class GameBoard:
 
 
 class Battleship:
+    
     def __init__(self, board):
         self.board = board
+    
+    def create_ships(self):
+        #creat five random ships for both the user and the computer
+        for ship in range(5):
+            self.row = random.randint(0, 4)
+            self.column = random.randint(0, 4)
+            while self.board[self.row][self.column] == "X":
+                self.row = random.randint(0, 4)
+                self.column = random.randint(0, 4)
+            self.board[self.row][self.column] = "X"
+        return self.board
 
-                   
+                 
 computer_board = GameBoard([[" "] * 5 for i in range(5)])
 GameBoard.print_board((computer_board))
